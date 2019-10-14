0abeb38806b8d777ce3e59f762f9ba154f9a0224
Marc Heemskerk <xxttrack@gmail.com>
2019-10-14 09:42:57 +0200

    update code

--- share data nodemcu/reading/reading.ino
+++ share data nodemcu/reading/reading.ino
@@ -1,20 +1,20 @@
 #include "config.h"
 
-#define FEED_OWNER "AIO_FEED_OWNER"
+#define FEED_OWNER "feedowner"
+#define SHARED_FEED "counter"
 
-AdafruitIO_Feed *sharedFeed = io.feed("counter", FEED_OWNER);
+AdafruitIO_Feed *sharedFeed = io.feed(SHARED_FEED, FEED_OWNER);
+
 
 void setup() {
-  
   Serial.begin(115200);
-
+  
   while(! Serial);
-
   Serial.print("Connecting to Adafruit IO");
+  
   io.connect();
-
   sharedFeed->onMessage(handleMessage);
-
+  
   while(io.status() < AIO_CONNECTED) {
     Serial.print(".");
     delay(500);
@@ -23,17 +23,13 @@ void setup() {
   Serial.println();
   Serial.println(io.statusText());
   sharedFeed->get();
-
 }
 
 void loop() {
   io.run();
-
 }
 
 void handleMessage(AdafruitIO_Data *data) {
-
   Serial.print("received <-  ");
   Serial.println(data->toInt());
-  
 }
